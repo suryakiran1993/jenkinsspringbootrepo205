fedc59f7dda72aa4b79b278f77d01dcb0b0c916f
J SURYA KIRAN <kiransurya93@kluniversity.in>
2025-08-05 12:43:21 +0530

    spring boot project v1.0

---
+++ src/main/java/com/klef/SpringBootDemoProjectApplication.java
@@ -0,0 +1,16 @@
+package com.klef;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SpringBootDemoProjectApplication extends SpringBootServletInitializer
+{
+	public static void main(String[] args) 
+	{
+		SpringApplication.run(SpringBootDemoProjectApplication.class, args);
+		System.out.println("Hi DevOps ...");
+	}
+
+}
