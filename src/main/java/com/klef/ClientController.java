7ba0f1c3c5d5b5f9b39eeb319d1ee56372c75f47
J SURYA KIRAN <kiransurya93@kluniversity.in>
2025-08-09 17:01:37 +0530

    updated project

--- src/main/java/com/klef/ClientController.java
+++ src/main/java/com/klef/ClientController.java
@@ -17,4 +17,10 @@ public class ClientController
    {
 	   return "Jenkins Demo";
    }
+   
+   @GetMapping("/test")
+   public String test()
+   {
+	   return "Deploy Spring Boot using Jenkins";
+   }
 }
